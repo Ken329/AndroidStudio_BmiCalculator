549c53df31a3b49b4a806624fbcd1970f3c3ed28
ken <ken_037729@hotmail.com>
2020-11-28 20:29:27 +0800

    Commit functionality

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvBmi;
+    TextView tvRes;
+    EditText kg;
+    EditText height;
+    Button enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvBmi = findViewById(R.id.textViewBmi);
+        tvRes = findViewById(R.id.textViewRes);
+        kg = findViewById(R.id.editTextKilo);
+        height = findViewById(R.id.editTextHeight);
+        enter = findViewById(R.id.btnEnter);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String myKg = kg.getText().toString();
+                String myHeight = height.getText().toString();
+
+                double bmi = calBmi(Double.valueOf(myKg), Double.valueOf(myHeight));
+                String result = calRes(bmi);
+
+                tvBmi.setText(String.valueOf(bmi));
+                tvRes.setText(result);
+            }
+        });
     }
+    private double calBmi(double kg, double he){
+        he = he/100; // this make cm into meter;
+        double result = kg/(he*he);
+        return result;
+    }
+    private String calRes(double bmi){
+        String result = "Normal";
+        if(bmi < 18.5){
+            result = "Underweight";
+        }else if(bmi > 24.9){
+            result = "Overweight";
+        }
+        return result;
+    }
+
 }
